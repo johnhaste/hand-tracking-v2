c5dbb561ccba0ae6e3c4cba5a7ae4f24c57c027a
John Haste <joaovictorspteixeira@gmail.com>
2022-05-24 16:22:25 -0300

    3 UI Buttons Scene

---
+++ Assets/ButtonInteractor.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ButtonInteractor : MonoBehaviour
+{
+
+    public TextMeshProUGUI simpleUIText; 
+
+    public void OnButton1Clicked()
+    {
+        simpleUIText.text = "Button1 is clicked";
+    }
+
+    public void OnButton2Clicked()
+    {
+        simpleUIText.text = "Button2 is clicked";
+    }
+
+
+    public void OnButton3Clicked()
+    {
+        simpleUIText.text = "Button3 is clicked";
+    }
+
+
+}
